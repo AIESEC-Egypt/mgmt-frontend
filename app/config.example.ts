6ea87bb9b2f74ba4f2e19bee86829d5d39674a76
Karl Johann Schubert <karljohann@familieschubi.de>
2016-06-29 19:26:52 +0200

    FIX: config.example.ts

--- app/config.example.ts
+++ app/config.example.ts
@@ -1,9 +1,9 @@
 export var config: any = {
     "auth": {
-        "callbackUrl" : "https://tasks.aiesec.org.eg/signin",
+        "callbackUrl" : "https://mgmt.aiesec.org.eg/signin",
         "implicitGrantUrl": "https://auth.aiesec.org.eg/index.php?action=authorize&redirect_uri=__callbackUrl__&response_type=token&client_id=__clientId__&scope=__scopes__",
         "userInfoUrl": "https://auth.aiesec.org.eg/index.php?action=current_person",
-        "clientId": "tasks-frontend",
+        "clientId": "mgmt-frontend",
         "scopes": "",
     },
     "API_BASE": "https://api.aiesec.org.eg/"
